925189d87379a89fcb4283f49fa3ea498acf7bf1
Joshua Lee <josh.lee134@hotmail.com>
2023-05-03 17:50:24 -0700

    change seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -40,78 +40,65 @@ require 'open-uri'
 #   puts "Done!"
 # end
 
-  # puts 'Destroying tables...'
-  # Product.destroy_all
+#   puts 'Destroying tables...'
+#   Product.destroy_all
 
-  # puts "Resetting primary keys..."
-  # ActiveRecord::Base.connection.reset_pk_sequence!('products')
+#   puts "Resetting primary keys..."
+#   ActiveRecord::Base.connection.reset_pk_sequence!('products')
 
-  # puts 'Seeding Products...'
+#   puts 'Seeding Products...'
 
-  # products = JSON.load_file "./nikecrawl/shoes.json", symbolize_names: true
+#   products = JSON.load_file "./nikecrawl/shoes.json", symbolize_names: true
 
-  # products.each do |product|
-  #   productHash = product.deep_transform_keys { |key| key.to_s.underscore.to_sym }
-  #   puts "Creating product: #{product[:name]} (#{product[:sku]})"
-  #   productHash[:sku] = productHash[:sku].downcase
-  #   product = Product.new(productHash.slice(:name, :title, :description, :color, :p_type, :sku, :category, :size))
-  #   product.price = productHash[:price][1..-1].to_f
-  #   product.save!
-  #   productHash[:images].each_with_index do |image_url, i|
-  #     puts "Attaching image: #{product.sku}_#{i}.png"
-  #     product.photos.attach(
-  #       io: URI.open(image_url), 
-  #       filename: "#{product.sku}_#{i}.png"
-  #     )
-  #   end
-  # end
+#   products.each do |product|
+#     productHash = product.deep_transform_keys { |key| key.to_s.underscore.to_sym }
+#     puts "Creating product: #{product[:name]} (#{product[:sku]})"
+#     productHash[:sku] = productHash[:sku].downcase
+#     product = Product.new(productHash.slice(:name, :title, :description, :color, :p_type, :sku, :category, :size))
+#     product.price = productHash[:price][1..-1].to_f
+#     product.save!
+#     productHash[:images].each_with_index do |image_url, i|
+#       puts "Attaching image: #{product.sku}_#{i}.png"
+#       product.photos.attach(
+#         io: URI.open(image_url), 
+#         filename: "#{product.sku}_#{i}.png"
+#       )
+#     end
+#   end
 
 products = Product.all
 numbers = (1..10).to_a
 
-# ApplicationRecord.transaction do
-#   puts "Destroying tables..."
-#   # Unnecessary if using `rails db:seed:replant`
-#   CartItem.destroy_all
-
-#   puts "Resetting primary keys..."
-#   ActiveRecord::Base.connection.reset_pk_sequence!('cart_items')
-
-#   puts "Creating new carts"
-
-#   20.times do 
-#     rand_product = products.sample
-#     rand_num = numbers.sample
-#     size = JSON.parse(rand_product.size).sample 
-#     CartItem.create(product_id: rand_product.id, quantity: rand_num, user_id: 1, size: size)
-#   end
-
-#   puts "Done!"
-
-# end
-
-# ApplicationRecord.transaction do
-#   puts "Destroying tables..."
-#   Favorite.destroy_all
-
-#   puts "Resetting Primary keys..."
-#   ActiveRecord::Base.connection.reset_pk_sequence!('favorites')
-
-#   puts "Creating Favorites..."
-#   20.times do
-#     rand_product = products.sample
-#     Favorite.create(product_id: rand_product.id, favoriter_id: 1)
-#   end
-
-#   puts "Done"
-# end
-
 ApplicationRecord.transaction do
   puts "Destroying tables..."
+  # Unnecessary if using `rails db:seed:replant`
+  CartItem.destroy_all
+  Favorite.destroy_all
   Review.destroy_all
 
-  puts "Resetting Primary keys..."
+  puts "Resetting primary keys..."
+  ActiveRecord::Base.connection.reset_pk_sequence!('cart_items')
+  ActiveRecord::Base.connection.reset_pk_sequence!('favorites')
   ActiveRecord::Base.connection.reset_pk_sequence!('reviews')
+
+
+
+  puts "Creating new carts..."
+
+  20.times do 
+    rand_product = products.sample
+    rand_num = numbers.sample
+    size = JSON.parse(rand_product.size).sample 
+    CartItem.create(product_id: rand_product.id, quantity: rand_num, user_id: 1, size: size)
+  end
+
+  puts "Creating Favorites..."
+  20.times do
+    rand_product = products.sample
+    Favorite.create(product_id: rand_product.id, favoriter_id: 1)
+  end
+
+  puts "Done!"
 
   puts "Creating Reviews..."
   5.times do
@@ -142,7 +129,5 @@ ApplicationRecord.transaction do
     )
   end
 
-  puts "Done"
 end
-
 
