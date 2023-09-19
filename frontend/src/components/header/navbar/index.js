89c11fd9acce0ebad0a9ba620705ca9803c17281
Joshua Lee <josh.lee134@hotmail.com>
2023-09-18 19:26:01 -0700

    create hamburger menu

--- frontend/src/components/header/navbar/index.js
+++ frontend/src/components/header/navbar/index.js
@@ -5,25 +5,33 @@ import {useSelector } from 'react-redux'
 import Navigation from '../../authentication/navigation'
 import CartFavorite from "./cartfavorite";
 import SearchFunction from "../search";
+import {FaBars} from 'react-icons/fa'
 import './navbar.css'
 
 const NavBar = () => {
   const sessionUser = useSelector(state => state.session.user);
   const location = useLocation();
 
+  const menuClick = () => {
+    
+  }
+
   return(
     <>
       <ul className="navbar">
         <Link to='/'><img className="navbar-logo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a6/Logo_NIKE.svg/2880px-Logo_NIKE.svg.png" alt="logo"/></Link>
-        {location.pathname == "/" ? <NavBarCategories /> : null}
+        {location.pathname === "/" ? <NavBarCategories /> : null}
         <div className="nav-right">
-        <div className="cart-favorite-icons">
-          {sessionUser ? <CartFavorite /> : null}
-        </div>
+          <div className="cart-favorite-icons">
+            {sessionUser ? <CartFavorite /> : null}
+          </div>
           {sessionUser ? <Navigation /> : <LoginOptions />}
-        <label className="search-function-container">
-          <SearchFunction />
-        </label>
+          <label className="search-function-container">
+            <SearchFunction />
+          </label>
+          <div className="hamburger-menu">
+            <FaBars onClick={() => menuClick()}/>
+          </div>
         </div>
       </ul>
     </>
